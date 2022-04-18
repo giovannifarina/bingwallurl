66840a0f10903142081e8a3183c90248234b3933
Giovanni Farina <gfarina@diag.uniroma1.it>
2022-04-18 20:34:59 +0200

    update

--- bing.rb
+++ bing.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'http'
+require 'nokogiri'
 
 get '/' do
-  "Hello World!"
+  response = HTTP.timeout(2).get("https://www.bing.com/HPImageArchive.aspx?idx=0&n=1&mkt=en-US")
+  xml_doc  = Nokogiri::XML(response.body)
+
+  img_url = xml_doc.css('urlBase').text
+  domain = 'https://www.bing.com'
+  suffix = '_UHD.jpg'
+
+  return domain + img_url + suffix
+  #puts(domain + img_url + suffix)
 end
